09c51e87e59fc5b6f90296ca7552176adbb44fbd
YOUNES SEDKI <younes_sedki@hotmail.fr>
2026-01-30 13:04:48 +0100

    Add webpack config to completely exclude problematic ES module packages from server bundle

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,30 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Completely exclude problematic packages from server-side bundle
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'html-encoding-sniffer': false,
+        '@exodus/bytes': false,
+        'jsdom': false,
+        'dompurify': false,
+        'isomorphic-dompurify': false,
+      }
+      
+      // Also add to externals to prevent bundling
+      config.externals = config.externals || []
+      config.externals.push({
+        'html-encoding-sniffer': 'commonjs html-encoding-sniffer',
+        '@exodus/bytes': 'commonjs @exodus/bytes',
+        'jsdom': 'commonjs jsdom',
+        'dompurify': 'commonjs dompurify',
+        'isomorphic-dompurify': 'commonjs isomorphic-dompurify',
+      })
+    }
+    return config
+  },
 }
 
 export default nextConfig

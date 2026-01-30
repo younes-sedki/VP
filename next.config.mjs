f60e0ae8a612854741b9459e765c8e4aed76b84e
YOUNES SEDKI <younes_sedki@hotmail.fr>
2026-01-30 12:58:26 +0100

    Remove dompurify and jsdom packages to fix ES module error completely

--- next.config.mjs
+++ next.config.mjs
@@ -16,21 +16,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Fix for ES module compatibility issues in serverless environment
-      config.externals = config.externals || []
-      config.externals.push({
-        'html-encoding-sniffer': 'commonjs html-encoding-sniffer',
-        '@exodus/bytes': 'commonjs @exodus/bytes',
-      })
-    }
-    return config
-  },
-  // Handle ES modules in serverless functions
-  experimental: {
-    serverComponentsExternalPackages: ['dompurify'],
-  },
 }
 
 export default nextConfig
